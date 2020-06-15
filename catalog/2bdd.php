9042d1abd7c72436a7ee35ec7e23c588c01feec0
themaire <n.elie@me.com>
2020-06-15 22:45:26 +0200

    Update 2bdd.php

--- catalog/2bdd.php
+++ catalog/2bdd.php
@@ -3,7 +3,7 @@
 	include("lib/fonctions/fonctions.php");
   
 	addBdd('models');
-	exec("mysql --user=root --password=" . DBPASSWD . "--database=lib3d -e 'delete from fichiers where fi_nom like '._%'";);
+	exec("mysql --user=root --password=" . DBPASSWD . "--database=lib3d -e 'delete from fichiers where fi_nom like '._%';");
 	delBdd(); 
 
 ?>
