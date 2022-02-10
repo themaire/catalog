83e24f0ec39bcd904df450466488f20b4c645aed
Themaire <n.elie@me.com>
2022-02-10 23:24:11 +0100

    Update 2bdd.php

--- catalog/2bdd.php
+++ catalog/2bdd.php
@@ -2,15 +2,15 @@
 	include("const.php");
 	include("lib/fonctions/fonctions.php");
   
-	$stlDirectory = '/models'	
+	$stlDirectory = 'models';	
 
 	// Empècher le scan si le dossier de base des STLs VIDE. Pour empêcher l'effacement complet de la BDD.
 	$dir = scanDoss($stlDirectory);
 
 	if(count($dir) != 0){
-	addBdd($stlDirectory);
-	exec("mysql --user=root --password=" . DBPASSWD . "--database=lib3d -e 'delete from fichiers where fi_nom like '._%';");
-	delBdd();
+		addBdd($stlDirectory);
+		exec("mysql --user=root --password=" . DBPASSWD . "--database=lib3d -e 'delete from fichiers where fi_nom like '._%';");
+		delBdd();
 	}
 	//else{ Prevenir ???}
 
